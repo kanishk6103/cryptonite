65d157b4937cab938456f8bb4117d8e212ad4656
kanishk6103 <mamta11tiwari06@gmail.com>
2024-07-21 21:38:39 +0530

    fixed a bug that prevented users from switching themes

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -33,7 +33,7 @@ export default function ThemeSwitch() {
     document.startViewTransition(switchTheme);
   };
 
-  if (!mounted)
+  if (!mounted) {
     return (
       <Image
         src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
@@ -45,6 +45,7 @@ export default function ThemeSwitch() {
         title="Loading Light/Dark Toggle"
       />
     );
+  }
 
   if (theme === "dark") {
     return <FiSun onClick={toggleTheme} />;
